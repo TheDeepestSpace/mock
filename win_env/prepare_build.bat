d86af19bb1d6cb75ac7b62e8aa6a0755ace5c813
Boris <into.the.deepest.space@gmail.com>
2018-10-27 13:49:14 -0400

    trigger appveyor build

--- win_env/prepare_build.bat
+++
@@ -1,4 +0,0 @@
-if not exist "c:\projects\mock\tmp" mkdir c:\projects\mock\tmp
-if not exist "c:\projects\mock\tmp\lib" mkdir c:\projects\mock\tmp\lib
-if not exist "c:\projects\mock\tmp\lib\include" mkdir c:\projects\mock\tmp\lib\include
-if not exist "c:\projects\mock\tmp\test" mkdir c:\projects\mock\tmp\test
