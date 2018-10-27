5b39b1bd2c657769feed4349d3e4808289b07ba4
Boris <into.the.deepest.space@gmail.com>
2018-10-27 13:47:33 -0400

    add vs solution files

---
+++ win_env/prepare_build.bat
@@ -0,0 +1,4 @@
+if not exist "c:\projects\mock\tmp" mkdir c:\projects\mock\tmp
+if not exist "c:\projects\mock\tmp\lib" mkdir c:\projects\mock\tmp\lib
+if not exist "c:\projects\mock\tmp\lib\include" mkdir c:\projects\mock\tmp\lib\include
+if not exist "c:\projects\mock\tmp\test" mkdir c:\projects\mock\tmp\test
